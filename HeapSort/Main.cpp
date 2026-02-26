924b7a95151d66859dfbc3f788b93a96457a94ab
ronniej <ronniej@naver.com>
2026-02-26 10:39:38 +0900

    힙 정렬 구현 및 데모 진행
    - main 함수에서 정렬 테스트 진행

--- HeapSort/Main.cpp
+++ HeapSort/Main.cpp
@@ -17,7 +17,7 @@ void Heapify(int* array, int length, int index)
 		int leftChild = 2 * index + 1;
 		// 오른쪽 자손: 2N + 2.
 		int rightChild = 2 * index + 2;
-		
+
 		// 더 큰값 크기 (+자손 인덱스 존재 여부 확인).
 		if (leftChild < length
 			&& array[leftChild] > array[largest])
@@ -55,7 +55,7 @@ void HeapSort(int* array, int length)
 	{
 		Heapify(array, length, ix);
 	}
-	
+
 	// 힙 정렬.
 	for (int ix = length - 1; ix > 0; --ix)
 	{
@@ -67,7 +67,52 @@ void HeapSort(int* array, int length)
 	}
 }
 
+// 배열 항목 출력 함수.
+void PrintArray(int* array, int length)
+{
+	for (int ix = 0; ix < length; ++ix)
+	{
+		std::cout << array[ix];
+
+		// 마지막 원소 전까지는 콤마 출력.
+		if (ix < length - 1)
+		{
+			std::cout << ", ";
+		}
+	}
+
+	std::cout << "\n";
+}
+
 int main()
 {
-	int array[] = { 5, 2, 8, 4, 1, 7, 3, 6, 9, 10, 15, 13, 14, 12, 17, 16 };
+	//int array[] = { 5, 2, 8, 4, 1, 7, 3, 6, 9, 10, 15, 13, 14, 12, 17, 16 };
+
+	int array[] =
+	{
+		1, 58, 10, 20, 56, 63, 73, 5, 28, 37,
+		80, 61, 82, 45, 11, 66, 83, 59, 22, 64,
+		52, 89, 94, 76, 44, 40, 75, 2, 23, 57,
+		92, 8, 41, 96, 15, 84, 35, 69, 54, 47,
+		90, 24, 43, 74, 34, 85, 72, 95, 18, 17,
+		98, 9, 29, 53, 27, 79, 39, 51, 31, 16,
+		6, 97, 26, 100, 21, 48, 33, 60, 91, 19,
+		30, 13, 71, 78, 87, 25, 81, 4, 42, 93,
+		49, 12, 14, 7, 62, 77, 38, 99, 88, 50,
+		32, 46, 70, 3, 86, 68, 36, 67, 55, 65
+	};
+
+	// 배열 길이 구하기.
+	int length = _countof(array);
+
+	// 출력.
+	PrintArray(array, length);
+
+	// 정렬.
+	HeapSort(array, length);
+
+	// 출력.
+	PrintArray(array, length);
+
+	std::cin.get();
 }
